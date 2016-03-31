35a53ca57908f5cdcdeb9edc4267d89bd97a111c
tahmina-ahmed <tahmina.csebuet@gmail.com>
2016-03-31 16:46:31 -0500

    Update policy.py

--- simplerebac/policy.py
+++ simplerebac/policy.py
@@ -1,5 +1,8 @@
 
 class Policy(object):
+        _path_dict = {}
+        _paths= []
+        _policy_dict= {}
         def check_policy(self, action_type, source_user, target_user):
             if action_type == 'addRelation':
                  return self.add_relationship_policy(source_user, target_user)
@@ -8,16 +11,22 @@ class Policy(object):
             elif action_type == 'access':
                  return self.access_policy(source_user, target_user)
 
-        def add_relationship_policy(self, source_user, target_user):
+        def add_relationship_policy(self, paths, source_user, target_user):
            """ check policy to add relationship between two users"""
-           paths=self.find_all_paths(source_user,target_user)
+           self._paths= paths
            return True
 
-        def delete_relationship_policy(self,source_user,target_user):
+        def delete_relationship_policy(self, paths, source_user,target_user):
            """ check  policy to delete relationship between two users"""
+           self._paths = paths 
            return True
 
-        def access_policy(self,source_user,target_user):
+        def access_policy(self, paths,source_user,target_user):
            """check policy to access target user"""
+           self._paths = paths
            return True
-
+           
+        def compute_path_dict():
+            for path in self._paths:
+                    self._path_dict[len(path)].append(path)
+                    
